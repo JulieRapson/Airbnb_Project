5928853e6836299d2559badd3231e48915a019ce
JulieRapson <rapson_julie@columbusstate.edu>
2023-02-15 14:21:39 -0500

    Project update without comments

--- AirbnbProject/frmAirBnb.Designer.vb
+++ AirbnbProject/frmAirBnb.Designer.vb
@@ -25,6 +25,13 @@ Partial Class frmAirBnb
         Me.picAirBnb = New System.Windows.Forms.PictureBox()
         Me.lblOrlandoBnb = New System.Windows.Forms.Label()
         Me.lblCost = New System.Windows.Forms.Label()
+        Me.btnCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtNumberofnights = New System.Windows.Forms.TextBox()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.lblCostofstay = New System.Windows.Forms.Label()
+        Me.lblCalcCost = New System.Windows.Forms.Label()
         CType(Me.picAirBnb, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -43,7 +50,7 @@ Partial Class frmAirBnb
         Me.lblOrlandoBnb.AutoSize = True
         Me.lblOrlandoBnb.Enabled = False
         Me.lblOrlandoBnb.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblOrlandoBnb.Location = New System.Drawing.Point(464, 41)
+        Me.lblOrlandoBnb.Location = New System.Drawing.Point(467, 37)
         Me.lblOrlandoBnb.Name = "lblOrlandoBnb"
         Me.lblOrlandoBnb.Size = New System.Drawing.Size(255, 37)
         Me.lblOrlandoBnb.TabIndex = 1
@@ -53,11 +60,76 @@ Partial Class frmAirBnb
         '
         Me.lblCost.AutoSize = True
         Me.lblCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCost.Location = New System.Drawing.Point(492, 100)
+        Me.lblCost.Location = New System.Drawing.Point(495, 96)
         Me.lblCost.Name = "lblCost"
         Me.lblCost.Size = New System.Drawing.Size(209, 24)
         Me.lblCost.TabIndex = 2
         Me.lblCost.Text = "Only $79.00 per night"
+        '
+        'btnCost
+        '
+        Me.btnCost.Location = New System.Drawing.Point(432, 335)
+        Me.btnCost.Name = "btnCost"
+        Me.btnCost.Size = New System.Drawing.Size(75, 23)
+        Me.btnCost.TabIndex = 3
+        Me.btnCost.Text = "Display Cost"
+        Me.btnCost.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(547, 334)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(647, 335)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtNumberofnights
+        '
+        Me.txtNumberofnights.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumberofnights.Location = New System.Drawing.Point(614, 191)
+        Me.txtNumberofnights.Name = "txtNumberofnights"
+        Me.txtNumberofnights.Size = New System.Drawing.Size(38, 24)
+        Me.txtNumberofnights.TabIndex = 6
+        '
+        'lblNumber
+        '
+        Me.lblNumber.AutoSize = True
+        Me.lblNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumber.Location = New System.Drawing.Point(432, 197)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(145, 18)
+        Me.lblNumber.TabIndex = 7
+        Me.lblNumber.Text = "Number of Nights:"
+        '
+        'lblCostofstay
+        '
+        Me.lblCostofstay.AutoSize = True
+        Me.lblCostofstay.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostofstay.Location = New System.Drawing.Point(432, 262)
+        Me.lblCostofstay.Name = "lblCostofstay"
+        Me.lblCostofstay.Size = New System.Drawing.Size(107, 18)
+        Me.lblCostofstay.TabIndex = 8
+        Me.lblCostofstay.Text = "Cost of Stay:"
+        '
+        'lblCalcCost
+        '
+        Me.lblCalcCost.AutoSize = True
+        Me.lblCalcCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCalcCost.Location = New System.Drawing.Point(611, 262)
+        Me.lblCalcCost.Name = "lblCalcCost"
+        Me.lblCalcCost.Size = New System.Drawing.Size(57, 18)
+        Me.lblCalcCost.TabIndex = 9
+        Me.lblCalcCost.Text = "Label1"
         '
         'frmAirBnb
         '
@@ -65,6 +137,13 @@ Partial Class frmAirBnb
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.PaleGoldenrod
         Me.ClientSize = New System.Drawing.Size(800, 461)
+        Me.Controls.Add(Me.lblCalcCost)
+        Me.Controls.Add(Me.lblCostofstay)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.txtNumberofnights)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCost)
         Me.Controls.Add(Me.lblCost)
         Me.Controls.Add(Me.lblOrlandoBnb)
         Me.Controls.Add(Me.picAirBnb)
@@ -80,4 +159,11 @@ Partial Class frmAirBnb
     Friend WithEvents picAirBnb As PictureBox
     Friend WithEvents lblOrlandoBnb As Label
     Friend WithEvents lblCost As Label
+    Friend WithEvents btnCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtNumberofnights As TextBox
+    Friend WithEvents lblNumber As Label
+    Friend WithEvents lblCostofstay As Label
+    Friend WithEvents lblCalcCost As Label
 End Class
--- AirbnbProject/frmAirBnb.vb
+++ AirbnbProject/frmAirBnb.vb
@@ -1,3 +1,36 @@
 ﻿Public Class frmAirBnb
+    ' Cost of a nights stay variable to use for calculations
+    Const decTotalCost As Decimal = 79D
+    Private Sub lblCalcCost_Click(sender As Object, e As EventArgs) Handles lblCalcCost.Click
 
+    End Sub
+
+    Private Sub frmAirBnb_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblCalcCost.Text = ""
+        txtNumberofnights.Text = ""
+        txtNumberofnights.Select()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnCost_Click(sender As Object, e As EventArgs) Handles btnCost.Click
+        Dim strNumNights As String
+        Dim intNumNights As Integer
+        Dim decTotalCostOfStay As Decimal
+
+        strNumNights = txtNumberofnights.Text
+        intNumNights = Convert.ToInt32(strNumNights)
+        decTotalCostOfStay = intNumNights * decTotalCost
+        lblCalcCost.Text = decTotalCostOfStay.ToString("C")
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblCalcCost.Text = ""
+        txtNumberofnights.Text = ""
+        txtNumberofnights.Select()
+    End Sub
 End Class
